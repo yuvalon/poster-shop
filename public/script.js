51b3f085473bbf6532373d65e048393d0007e461
leyndarmal <leydarmal20@gmail.com>
2019-01-03 00:25:57 +0200

    finished - up and running with vue

--- public/script.js
+++ public/script.js
@@ -1,1 +1,16 @@
-console.log('It works.');
+new Vue({
+    el: '#app',
+    data:{
+        total: 0,
+        items: [
+            {title: 'item 1'},
+            {title: 'item 2'},
+            {title: 'item 3'}
+        ]
+    },
+    methods:{
+        addItem: function (){
+            this.total += 9.99;
+        }
+    }
+})
